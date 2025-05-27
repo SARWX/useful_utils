569804041d84c0ab0bc14c9cfd6e9b06e05d7e3b
Timofei Novikov <tnovikov@astralinux.ru>
2025-05-27 16:23:25 +0300

    diff_configs aimed to automate making diff's for chaned configs

---
+++ diff_configs.sh
@@ -0,0 +1,40 @@
+#/bin/bash
+
+original_branch=$(git rev-parse --abbrev-ref HEAD)
+debian/rules quilt-push >/dev/null 2>&1
+
+echo "NEW GENERIC"
+debian/rules clean >/dev/null 2>&1
+yes "" | debian/rules config-generic >/dev/null 2>&1
+cp debian/build/.config ../new_generic.config
+
+echo "NEW DEBUG"
+debian/rules clean >/dev/null 2>&1
+yes "" | debian/rules config-debug >/dev/null 2>&1
+cp debian/build/.config ../new_debug.config
+
+git checkout HEAD~1 >/dev/null 2>&1
+
+echo "OLD GENERIC"
+debian/rules clean >/dev/null 2>&1
+yes "" | debian/rules config-generic >/dev/null 2>&1
+cp debian/build/.config ../old_generic.config
+
+echo "OLD DEBUG"
+debian/rules clean >/dev/null 2>&1
+yes "" | debian/rules config-debug >/dev/null 2>&1
+cp debian/build/.config ../old_debug.config
+
+debian/rules quilt-pop >/dev/null 2>&1
+git checkout "$original_branch"
+
+echo "========== old debug vs new debug =========="
+diff ../old_debug.config ../new_debug.config --color
+
+echo "========== old generic vs new debug =========="
+diff ../old_debug.config ../new_debug.config --color
+
+rm ../new_debug.config
+rm ../old_debug.config
+rm ../new_generic.config
+rm ../old_generic.config
